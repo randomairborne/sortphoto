b73de75ab899cf47fb7efba61102974844ac4278
valkyrie_pilot <valk@randomairborne.dev>
2022-10-08 14:04:51 -0600

    Fix mac packaging

--- packaging/mac/build.sh
+++ packaging/mac/build.sh
@@ -5,8 +5,8 @@ cargo b --release --target aarch64-apple-darwin
 echo "Building for x86 mac"
 cargo b --release --target x86_64-apple-darwin
 echo "Creating universal binary executable"
-lipo -create -output ./apple-package/bundle/Contents/MacOS/sortphoto ./target/x86_64-apple-darwin/release/sortphoto ./target/aarch64-apple-darwin/release/sortphoto
+lipo -create -output ./packaging/mac/bundle/Contents/MacOS/sortphoto ./target/x86_64-apple-darwin/release/sortphoto ./target/aarch64-apple-darwin/release/sortphoto
 echo "Copying bundle to app file"
-cp -r ./apple-package/bundle ./apple-package/sortphoto.app
+cp -r ./packaging/mac/bundle ./packaging/mac/sortphoto.app
 echo "Generating installer package"
-pkgbuild --install-location /Applications --component ./apple-package/sortphoto.app ./apple-package/sortphoto.pkg
+pkgbuild --install-location /Applications --component ./packaging/mac/sortphoto.app ./packaging/mac/sortphoto.pkg
